14becacdfe2f93ab659c28750df54f8512dcabba
FabricioMQ <fabriciomq1@gmail.com>
2023-06-17 17:32:51 -0600

    Modulo 1
    Co-authored-by: Jordy Vargas <JordyV23@users.noreply.github.com>
    Co-authored-by: Zaylin Priscilla Chaves Pérez <ChavesZay@users.noreply.github.com>
    Co-authored-by: Luis Fernando <LuisFernandoSG@users.noreply.github.com>

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
+from django.urls import path, include
+from .Modulo1 import Views
 
 urlpatterns = [
-    path('/cbase',),
+    path('api/cbase/',Views.my_view)
+    #path('api/cbase/',Views.my_view),
 ]
