0a9ca6dda8ff85935b431f0576c9366881b346b4
FabricioMQ <fabriciomq1@gmail.com>
2023-06-20 23:01:35 -0600

    Allowed hosts

--- calculadora/settings.py
+++ calculadora/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3%(w*7d+af^)yyd^$n9ut4dwu2tit9%vl+=v^(ik4ts5i&ix_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'geppettoapi.up.railway.app']
 
