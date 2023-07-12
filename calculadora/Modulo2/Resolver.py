ffc8ecb67b28caea781d791decf9c54e5d456ae6
FabricioMQ <fabriciomq1@gmail.com>
2023-07-11 20:26:14 -0600

    Modulo 2 desarrollo

--- calculadora/Modulo2/Resolver.py
+++ calculadora/Modulo2/Resolver.py
@@ -1,42 +1,40 @@
-from sympy import symbols, solve 
+import json
 import re
+import wolframalpha
+from decouple import config
 
 def encontrar_variables_numeros_bien_escritos(request):
     expresion_latex=request.data.get("expresion_latex")
     numeros = expresion_latex.split(";")
     paso1 = []
-    incognitas=[]#es temporal
 
-    #paso 1 leer el latex
+    #paso 1 leer el latex 
     for num in numeros:
         digitos, base = obtener_digitos_y_bases(num.strip())
         paso1.append({"num":digitos,"base":base})
-    
-    #paso 2 hacer la operacion logica
-    #en este apartado se debe que crear la logica de cada num
-    for num in paso1:
-      if num["num"] is not None and num["base"] is not None:
-        digitos = num["num"]
-        base = num["base"]
 
-        # Verificar si los dígitos contienen letras o son variables
-        if re.search(r"[a-z]", digitos):
-            incognitas.append(digitos)
+ 
+    ecuaciones_latex ="2+2"
+    # Crea una instancia del cliente de Wolfram Alpha
+    cliente_wolfram = wolframalpha.Client(config("wolframalphaID"))
 
-        # Verificar si la base contiene letras o es una variable
-        if re.search(r"[a-z]", base):
-            incognitas.append(base)
+    # Envía la consulta a Wolfram Alpha
+    respuesta = cliente_wolfram.query(ecuaciones_latex)
 
-        # Verificar si la base es un número válido
-        if base.isdigit():
-            if 2 <= int(base) <= 62:
-                incognitas.append(base)
+    resultado = None
+    for pod in respuesta.results:
+        if pod["@title"] == "Integer solution":
+            resultado = pod["subpod"]["plaintext"]
+            break
 
-    return incognitas
+    if resultado is not None:
+       return {"Status":"True","Mensaje":"Numero bien escritos","solucion":resultado}
+    else:
+     return  {"Status":"false","Mensaje":"Error verificar si son numeros bien escritos"}
 
 def obtener_digitos_y_bases(numero_latex):
     # Buscar el patrón de dígitos y base en el número LaTeX
-    patron = r"\\overline{([a-fA-F0-9]+)}_{\(([a-fA-F0-9])\)}"
+    patron = r"\\overline{([a-zA-Z0-9]+)}_{\(([a-zA-Z0-9])\)}"
     coincidencias = re.search(patron, numero_latex)
 
     if coincidencias:
