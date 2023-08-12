fbb92000f575b7cf6cf8f95d60dd26fcbe54cd7c
FabricioMQ <fabriciomq1@gmail.com>
2023-08-11 18:01:07 -0600

    AI  PROMPT CONTEXTO

--- Calculadora/Modulo3/aiResolver.py
+++ Calculadora/Modulo3/aiResolver.py
@@ -2,17 +2,27 @@ import openai
 from decouple import config
 openai.api_key=config('OPENAI_API_KEY',default='')
 
-def resolver(request) :
-    try :
-       contenido=request.data
-       prompt=contenido.get("prompt")
-       if prompt is None: 
-            return  {"Status":"false","Mensaje":"Error al resolver ,el prompt esta nulo"}
-       else:    
-            return {"Status":"true","Mensaje":"Problema matematicos sistema numerico","Respuesta":__enviar_prompt__(prompt)}
-    except Exception as e:
-        return  {"Status":"false","Mensaje":"Error al resolver , verificar si los datos cumplen el formato"}
+def resolver(request):
+    try:
+        prompt_context = """
+        Contexto: Resolución de problemas reales con conversión de sistemas de numeración.
 
+        Nota: Si recibe un hola indica al usuario cual es el problema que necesita resolver y si recibe otra cosa que no es sobre este tema o te piden conceptos de otra cosa indica que solo puede resolver cosas relacionatas con el contexto
+
+        Por favor, resuelve el siguiente problema relacionado con la conversión de sistemas de numeración:
+        """
+
+        contenido = request.data
+        prompt = contenido.get("prompt")
+
+        if prompt is None:
+            return {"Status": "false", "Mensaje": "Error al resolver, el prompt está nulo"}
+        else:
+            full_prompt = prompt_context + "\n\n" + prompt
+            respuesta = __enviar_prompt__(full_prompt)
+            return {"Status": "true", "Mensaje": "Problema matemático de sistema numérico", "Respuesta": respuesta}
+    except Exception as e:
+        return {"Status": "false", "Mensaje": "Error al resolver, verificar si los datos cumplen el formato"}
 
 
 """
@@ -22,7 +32,7 @@ def resolver(request) :
 -frequency_penalty para que no quede en un bucle infinito con el mismo prompt
 -presence_penality para que no siga la conversacion o sacar otros temas
 """
-def __enviar_prompt__(prompt,engine='text-davinci-003',temp=1,max_tokens=100,top_p=1,frequency_penalty=1,presence_penalty=2):
+def __enviar_prompt__(prompt,engine='text-davinci-003',temp=0.1,max_tokens=100,top_p=1,frequency_penalty=1,presence_penalty=2):
     respuesta = openai.Completion.create(
                                             engine=engine,
                                             prompt=prompt,
